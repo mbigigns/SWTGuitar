fed14b5d5d10f5f6c1e1eefcb963c8766477d400
Eric Oliver <olivere@umd.edu>
2012-03-09 11:44:17 -0500

    Adding initial EFG parsing and tracing functionality

---
+++ SWTVisualization/src/efg/WidgetId.java
@@ -0,0 +1,37 @@
+package efg;
+
+public class WidgetId {
+
+	private long id;
+	
+	public WidgetId(String id) {
+		if (id.startsWith("w")) { id = id.substring(1, id.length()); }
+		this.id = Long.parseLong(id);
+	}
+	
+	public WidgetId(long id) { 
+		this.id = id;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	@Override public int hashCode() {
+		return ((Long)id).hashCode();
+	}
+	
+	@Override public boolean equals(Object o) {
+		if (o == null) { return false; }
+		
+		if (!WidgetId.class.isAssignableFrom(o.getClass())) { return false; }
+		
+		WidgetId other = (WidgetId) o;
+		
+		return this.id == other.id;
+	}
+	
+	@Override public String toString() {
+		return "w" + Long.toString(this.id);
+	}
+}
