46ef1ee474c849a304a318fb1d8a330fb7ba872c
Mike <mike@ubuntu.ubuntu-domain>
2012-03-19 15:23:39 -0400

    update vis run script

--- runner.sh
+++ runner.sh
@@ -1,13 +1,9 @@
-rm ./GUITAR-Default.EFG
-rm ./GUITAR-Default.GUI
-#rm ./GUITAR-Default.tst
-
 cp $1 GUITAR-Default.EFG
 cp $2 GUITAR-Default.GUI
 
 if ["$3" = ""]; then
 	java -jar Vis.jar
 else
-	cp $3 GUITAR-Default.tst
+	cp $3 testcase.tst
 	java -jar Vis.jar $3
 fi
